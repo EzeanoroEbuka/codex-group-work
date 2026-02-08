3df1d18102d25c59955ca08a40ba3a0df095b2de
Sammyxxtech <omosefunmisammy@gmail.com>
2026-02-08 11:36:03 +0100

    third commit

--- SAMUEL/student.py
+++ SAMUEL/student.py
@@ -1,5 +1,9 @@
 
 university_record = []
+available_courses = {"Math", "Physics", "Computer Science", "Biology", "Chemistry",
+                    "Statistics", "English", "Economics", "History", "Philosophy",
+                    "Sociology", "Political Science", "Geography", "Psychology", "Art",
+                    "Music", "Engineering", "Law", "Medicine", "Business"}
 
 def create_student(student_name, student_age, student_id, city, zip_code):
     student = {
@@ -16,6 +20,12 @@ def create_student(student_name, student_age, student_id, city, zip_code):
         }
     }
     university_record.append(student)
+
+def course_available(course):
+    if course in available_courses:
+        return True
+    return "Course is not available in department"
+
 
 def get_student_by_id(student_id):
     for student in university_record:
@@ -42,15 +52,17 @@ def update_student_city(student_id, student_new_city):
 def update_student_zip_code(student_id, student_new_zip_code):
     get_student_by_id(student_id)["student_info"]["Address"]["Zip_code"] = student_new_zip_code
 
-def add_student_courses(student_id, student_new_courses):
-    if check_student_has_course(student_id, student_new_courses):
+def add_student_courses(student_id, student_new_course):
+    if check_student_has_course(student_id, student_new_course):
         return "course already exists"
-    student_courses = get_student_by_id(student_id)["student_info"].get("Courses")
-    student_courses.add(student_new_courses)
-    return "course added successfully"
+    elif course_available(student_new_course) == True:
+        student_courses = get_student_by_id(student_id)["student_info"].get("Courses")
+        student_courses.add(student_new_course)
+        return "course added successfully"
+    return "course not available for student department"
 
 def update_student_course(student_id, old_course, student_new_course):
-    if check_student_has_course(student_id, old_course):
+    if check_student_has_course(student_id, old_course) and course_available(student_new_course):
         student_courses = get_student_by_id(student_id)["student_info"].get("Courses")
         student_courses.remove(old_course)
         student_courses.add(student_new_course)
@@ -91,11 +103,10 @@ university_record.append(student_info)
 #
 update_student_name("id","Ayobami")
 update_student_age("id",90)
-add_student_courses("id","Mathematics")
-print(add_student_courses("id","English"))
+print(add_student_courses("id","nglish"))
 print(add_student_courses("id","English"))
 
 # print(delete_student_course("id","yoruba"))
 
-
+# print(course_available("Math"))
 
