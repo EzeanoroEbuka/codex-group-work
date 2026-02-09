fff2d8230de30702edd7d0edebe43c18484def47
Oladayo234 <oladayoolubunmi20@gmail.com>
2026-02-09 13:27:13 +0100

    second commit

--- DAYO/school_management_system/School_management_system.py
+++ DAYO/school_management_system/School_management_system.py
@@ -1,3 +1,35 @@
-print("Hello World")
-print("Hello World")
-print("Hello World")
+COURSE_LIST = {"Math", "Physics", "Computer Science", "Biology", "Chemistry",
+"Statistics", "English", "Economics", "History", "Philosophy",
+"Sociology", "Political Science", "Geography", "Psychology", "Art",
+"Music", "Engineering", "Law", "Medicine", "Business"}
+
+all_student_records = {}
+
+def create_student(username, name, age, student_course, city, zip_code):
+    all_student_records[username] = {"name": name, "age": age, "courses": student_course,
+        "address": { "city": city, "zip_code": zip_code}
+    }
+    return all_student_records
+
+def get_student_records(username):
+    if username in all_student_records:
+        return username, all_student_records[username]
+    return None
+
+def get_student_courses(username):
+    if username in all_student_records:
+        return username, all_student_records["courses"]
+    return None
+
+def get_student_zip_codes(username):
+    if username in all_student_records:
+        return username, all_student_records ["address"]["zip_codes"]
+    return None
+
+def get_student_city(username):
+    if username in all_student_records:
+        return username, all_student_records["address"]["city"]
+    return None
+
+def add_course_for_student(username, course, student_records):
+    if username in all_student_records:
